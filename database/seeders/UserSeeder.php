b05e882ee4e077e78743140cc10b1ba0df0fd66f
randaru09 <ndaruanggoro123@gmail.com>
2025-02-18 14:28:10 +0700

    feat: add user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [
+            [
+                'name' => 'Supervisor',
+                'email' => 'spv@email.com',
+                'password' => 'spv12345',
+                'npp' => '11111',
+                'npp_supervisor' => null,
+            ],
+            [
+                'name' => 'Ananda Bayu',
+                'email' => 'bayu@email.com',
+                'password' => 'bayu1234',
+                'npp' => '12345',
+                'npp_supervisor' => '11111',
+            ],
+        ];
+
+        foreach ($data as $key => $item) {
+            User::create($item);
+        }
+    }
+}

c0fbdce6ccff997d7017daf84f65a93e3ba7b99a
randaru09 <ndaruanggoro123@gmail.com>
2025-02-18 19:43:38 +0700

    feat: add route to get epresence

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Epresence\ApproveEpresenceController;
 use App\Http\Controllers\Epresence\CreateEpresenceController;
+use App\Http\Controllers\Epresence\GetEpresenceByUserIdController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
@@ -12,4 +13,5 @@ Route::prefix('auth')->group(function () {
 Route::prefix('epresence')->middleware('jwt')->group(function () {
     Route::post('create', [CreateEpresenceController::class, 'action'] );
     Route::put('approve', [ApproveEpresenceController::class, 'action'] );
+    Route::get('get', [GetEpresenceByUserIdController::class, 'action'] );
 });

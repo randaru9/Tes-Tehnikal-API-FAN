965bbbf8c7961ca4c4fe0f45bb21ab622d40b1a0
randaru09 <ndaruanggoro123@gmail.com>
2025-02-18 17:44:24 +0700

    feat: add route for create epresence

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Epresence\CreateEpresenceController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
     Route::post('login', [LoginController::class, 'action'] );
+});
+
+Route::prefix('epresence')->middleware('jwt')->group(function () {
+    Route::post('create', [CreateEpresenceController::class, 'action'] );
 });
